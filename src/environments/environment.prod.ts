787a59f6e9e028e6d891733d8e98495ee151ca53
otisbrower <otisbrower@users.noreply.github.com>
2019-01-22 20:40:05 -0600

    first full project commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB5Xtf67_tLhGzk4cHqWReT3z4Pc52SKQ4',
+    authDomain: 'oshop-183c0.firebaseapp.com',
+    databaseURL: 'https://oshop-183c0.firebaseio.com',
+    projectId: 'oshop-183c0',
+    storageBucket: 'oshop-183c0.appspot.com',
+    messagingSenderId: '479371800596'
+  }
+};
